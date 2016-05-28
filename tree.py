959c9559b5eaa797f4908c84fdb2ee61bc966734
roboticmonkey <aiden.ward@gmail.com>
2016-05-28 16:41:10 -0700

    skills done

---
+++ tree.py
@@ -0,0 +1,112 @@
+"start time: 3:30pm end time: 4:00pm"
+"""Tree class and tree node class."""
+
+
+class Node(object):
+    """Node in a tree."""
+
+    def __init__(self, data, children=None):
+        children = children or []
+        assert isinstance(children, list), \
+            "children must be a list!"
+        self.data = data
+        self.children = children
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return "<Node %s>" % self.data
+
+    def get_num_children(self):
+        """Get number of children.
+
+        For example::
+
+            >>> a = Node("A", [Node("B"), Node("C")])
+            >>> a.get_num_children()
+            2
+        """
+
+        # FIXME
+        num_chil = len(self.children)
+
+        # pass
+        return num_chil
+
+
+class Tree(object):
+    """Tree."""
+
+    def __init__(self, root):
+        self.root = root
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return "<Tree root=%s>" % self.root
+
+    def depth_first_search(self, data):
+        """Return node object with this data, traversing the tree depth-first.
+
+        Start at the root, and return None if not found.
+        """
+
+        # FIXME
+        to_visit = [self.root]
+
+        while to_visit:
+            node = to_visit.pop()
+            print node
+
+            if node.data == data:
+                return node
+
+            to_visit.extend(node.children)
+
+        # pass
+        return None
+
+    def breadth_first_search(self, data):
+        """Return node object with this data, traversing the tree breadth-first.
+
+        Start here (on this node), and return None if not found.
+
+        Let's make a tree where we have two "B" nodes, but where one is far down an
+        earlier branch and the other is higher-up in an earlier branch. Since this is
+        a BFS, we should find the b2 node for "B"::
+
+                       A
+                     /   \
+                    C     E
+                   /       \
+                  D        B2
+                 /
+                B1
+
+            >>> a = Node("A")
+            >>> b1 = Node("B")
+            >>> b2 = Node("B")
+            >>> c = Node("C")
+            >>> d = Node("D")
+            >>> e = Node("E")
+            >>> a.children = [c, e]
+            >>> c.children = [d]
+            >>> d.children = [b1]
+            >>> e.children = [b2]
+            >>> tree = Tree(a)
+
+            >>> tree.depth_first_search("B") is b2
+            True
+
+        """
+
+        to_visit = [self.root]
+
+        while to_visit:
+            node = to_visit.pop(0)
+            print node
+
+            if node.data == data:
+                return node
+
+            to_visit.extend(node.children)
